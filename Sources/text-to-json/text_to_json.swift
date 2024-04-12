96051aa34238ec5f4fd39562aaeae060daa5bf32
a-matyukh <amatyukh@gmail.com>
2024-04-12 18:28:46 +0300

    basic ai generation

--- Sources/text-to-json/text_to_json.swift
+++ Sources/text-to-json/text_to_json.swift
@@ -1,6 +1,18 @@
 import Foundation
 import llmfarm_core
 
+let maxOutputLength = 256
+var total_output = 0
+
+func mainCallback(_ str: String, _ time: Double) -> Bool {
+    print("\(str)",terminator: "")
+    total_output += str.count
+    if(total_output>maxOutputLength) {
+        return true
+    }
+    return false
+}
+
 public func generate_steps(from: String, modelPath: String, grammarPath: String) -> [GeneratedStep] {
     
     print("Get this subtitles:")
@@ -8,7 +20,21 @@ public func generate_steps(from: String, modelPath: String, grammarPath: String)
     
     print(modelPath)
     let ai = AI(_modelPath: modelPath, _chatName: "chat")
-    print(ai)
+    
+    var params:ModelAndContextParams = .default
+    params.promptFormat = .Custom
+    params.custom_prompt_format = """
+    SYSTEM: You are a helpful, respectful and honest assistant.
+    USER: {prompt}
+    ASSISTANT:
+    """
+    var input_text = "State the meaning of life"
+    params.use_metal = true
+
+    _ = try? ai.loadModel_sync(ModelInference.LLama_gguf,contextParams: params)
+
+    let output = try? ai.model.predict(input_text, mainCallback)
+    
 //    print(grammarPath)
     
     
