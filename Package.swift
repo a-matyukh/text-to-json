a6e98ddfbe773e3b6cb81edf9f74182e1b94a72b
a-matyukh <amatyukh@gmail.com>
2024-04-12 16:46:43 +0300

    return decoded steps struct

--- Package.swift
+++ Package.swift
@@ -5,17 +5,26 @@ import PackageDescription
 
 let package = Package(
     name: "text-to-json",
+    platforms: [
+        .iOS(.v16)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "text-to-json",
             targets: ["text-to-json"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/guinmoon/llmfarm_core.swift.git", branch: "main")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "text-to-json"),
+            name: "text-to-json",
+            dependencies: [
+                .product(name: "llmfarm_core", package: "llmfarm_core.swift"),
+        ]),
         .testTarget(
             name: "text-to-jsonTests",
             dependencies: ["text-to-json"]),
--- Sources/text-to-json/text_to_json.swift
+++ Sources/text-to-json/text_to_json.swift
@@ -1,13 +1,25 @@
-public func generate_steps(from: String) -> String {
+import Foundation
+import llmfarm_core
+
+public func generate_steps(from: String) -> Steps {
     
     print("Get this subtitles:")
     print(from)
     
-    let generated_json = """
-    {steps: [{"step_name": "wau","step_description": "camon"}]}
-    """
-    
-    return generated_json
+    do {
+        let generated_json = """
+        {"steps": [{"step_name": "wau","step_description": "camon"}]}
+        """.data(using: .utf8)!
+        
+        let decodedSteps = try JSONDecoder().decode(Steps.self, from: generated_json)
+        print(decodedSteps)
+        return decodedSteps
+    } catch {
+        print("Check out failed: \(error.localizedDescription)")
+    }
+
+    let step = Step(step_name: "Something went wrong", step_description: "Try again")
+    return Steps(steps: [step])
     
 }
 
